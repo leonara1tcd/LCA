4a441992c83753fb7cf18992baac4e28d9616c5d
alexleonard <alexleonard@192.168.0.108>
2019-10-01 10:36:51 +0100

    Ran first few tests

--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -1,12 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.Test;
 
-class LCATest {
+public class LCATest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testNodeConstructor() {
+		Node tmp = new Node(1);
+		assertNotNull("Testing Node constructor: ", tmp);
 	}
 
+	@Test
+	public void testEmptyTree() {
+		LCA lca = new LCA();
+		assertNull("Find LCA when tree is empty: ", lca.findLCA(1, 4));
+		assertEquals("Find LCA when tree is empty: ", null, lca.findLCA(1, 4));
+	}
+
+	@Test
+	public void testCommon() {
+		// test tree structure:
+		// 1
+		// 2 3
+		// 4 5 6 7
+		LCA lca = new LCA();
+		lca.root = new Node(1);
+		lca.root.left = new Node(2);
+		lca.root.right = new Node(3);
+		lca.root.left.left = new Node(4);
+		lca.root.left.right = new Node(5);
+		lca.root.right.left = new Node(6);
+		lca.root.right.right = new Node(7);
+
+		assertEquals("LCA of 2 and 3: ", 1, lca.findLCA(2, 3).data);
+		assertEquals("LCA of 6 and 7: ", 3, lca.findLCA(6, 7).data);
+		assertEquals("LCA of 4 and 5: ", 2, lca.findLCA(4, 5).data);
+		assertEquals("LCA of 4 and 7: ", 1, lca.findLCA(4, 7).data);
+	}
 }
