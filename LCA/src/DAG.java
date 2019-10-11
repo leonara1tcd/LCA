d778582e4e6e0c17bbda85049ec683ed45193337
alexleonard <alexleonard@localhost>
2019-10-11 11:38:16 +0100

    New implementation for DAG. LCA class expanded

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,4 +1,175 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class DAG {
+	private int V; // no. of vertices
+	private int E; // no. of edges
+	private ArrayList<Integer>[] adj; // adjacency list for vertex v
+	private int[] indegree; // indegree of vertex v
+	private boolean marked[]; // Boolean array to track visited vertices
+	private boolean hasCycle; // True if graph has cycle
+	private boolean stack[]; // Order that vertices were visited
+	private int[] edgeTo; // edgeTo[v] = last edge on shortest s->v path
+	private int[] distTo; // distTo[v] = length of shortest s->v path
+
+	public DAG(int V) {
+		if (V < 0)
+			throw new IllegalArgumentException("Number of vertices in digraph must be nonnegative");
+		this.V = V;
+		this.E = 0;
+		indegree = new int[V];
+		marked = new boolean[V];
+		stack = new boolean[V];
+		adj = (ArrayList<Integer>[]) new ArrayList[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new ArrayList<Integer>();
+		}
+	}
+
+	// Return current vertex
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+	// Adds directed edge from v->w
+	public void addEdge(int v, int w) {
+		if ((validateVertex(v) > 0) && (validateVertex(w) > 0)) {
+			adj[v].add(w);
+			indegree[w]++;
+			E++;
+		} else {
+			System.out.println("Please enter vertices between 0 & n-1");
+		}
+
+	}
+
+	private int validateVertex(int v) {
+		if (v < 0 || v >= V)
+			return -1;
+		else
+			return 1;
+	}
+
+	// Return amount of directed edges incident to vertex v
+	public int indegree(int v) {
+		if (validateVertex(v) < 0) {
+			return -1;
+		} else {
+			return indegree[v];
+		}
+	}
+
+	// Return amount of directed edges from vertex v
+	public int outdegree(int v) {
+		if (validateVertex(v) < 0) {
+			return -1;
+		} else {
+			return adj[v].size();
+		}
+	}
+
+	// Returns the adjacent vertices to v
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+
+	public int findLCA(int v, int w) {
+		findCycle(0);
+		if (hasCycle) {
+			// Graph is not a DAG
+			return -1;
+		}
+		// Reverse the DAG, allowing easier traversal
+		DAG backwards = reverse();
+
+		// locate the two points in the graph
+		ArrayList<Integer> vPath = backwards.BFS(v);
+		ArrayList<Integer> wPath = backwards.BFS(w);
+		ArrayList<Integer> commonAncestors = new ArrayList<Integer>();
+		boolean found = false;
+
+		// cycling through the BFS paths, adding all common ancestors to the arrayList
+		// return the first one found, as it is closest to the nodes
+		for (int i = 0; i < vPath.size(); i++) {
+			for (int t = 0; t < wPath.size(); t++) {
+				if (vPath.get(i) == wPath.get(t)) {
+					commonAncestors.add(vPath.get(i));
+					found = true;
+				}
+			}
+		}
+		if (found)
+			return commonAncestors.get(0);
+		else
+			return -1;
+	}
+
+	// to find the LCA, will have to traverse the graph backwards
+	public DAG reverse() {
+		DAG reverse = new DAG(V);
+		for (int v = 0; v < V; v++) {
+			for (int w : adj(v)) {
+				reverse.addEdge(w, v); // reverse the direction of the edges
+			}
+		}
+		return reverse;
+	}
+
+	public ArrayList<Integer> BFS(int s) {
+		// Mark all the vertices as not visited(By default set as false)
+		boolean visited[] = new boolean[V];
+
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		ArrayList<Integer> order = new ArrayList<Integer>();
+
+		visited[s] = true;
+		queue.add(s);
+
+		while (queue.size() != 0) {
+			s = queue.poll();
+			order.add(s);
+			// Get all adjacent vertices of the dequeued vertex s
+			// If a adjacent has not been visited, then mark it
+			// and enqueue it
+			Iterator<Integer> i = adj[s].listIterator();
+			while (i.hasNext()) {
+				int n = i.next();
+				if (!visited[n]) {
+					visited[n] = true;
+					queue.add(n);
+				}
+			}
+		}
+
+		return order;
+
+	}
+
+	public boolean hasCycle() {
+
+		return hasCycle;
+	}
+
+	public void findCycle(int v) {
+
+		marked[v] = true;
+		stack[v] = true;
+
+		for (int w : adj(v)) {
+			if (!marked[w]) {
+				findCycle(w);
+			} else if (stack[w]) {
+				hasCycle = true;
+				return;
+			}
+		}
+
+		stack[v] = false;
+	}
 
 }
--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,28 +1,303 @@
-public class LCA {
+import java.util.ArrayList;
 
-	Digraph digraph;
+public class LCA<Key extends Comparable<Key>, Value> {
 
-	public LCA(Digraph g) {
-		this.digraph = g;
+	Node root;
+
+	class Node {
+		private Key key;
+		private Value val;
+		private Node left, right;
+		private int N;
+
+		public Node(Key key, Value val, int N) {
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+
 	}
 
-	public boolean hasCycle() {
-		DirectedCycle testDAG = new DirectedCycle(digraph);
-		return testDAG.hasCycle();
-	}
+	class DAG {
+		private Node root;
 
-	public int lowestCommonAncestor(int a, int b) {
-		if (hasCycle()) {
-			return -1;
-		} else if (digraph.V() == 0) {
-			return -1;
-		} else {
-			return 0;
+		public DAG(Node root) {
+			this.root = root;
 		}
 	}
 
-	public static void main(String[] rgs) {
-		Digraph v = new Digraph(2);
-		v.addEdge(0, 1);
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	// return number of key-value pairs in BST
+	public int size() {
+		return size(root);
+	}
+
+	// return number of key-value pairs in BST rooted at x
+	private int size(Node x) {
+		// BST is empty
+		if (x == null)
+			return 0;
+
+		else
+			return x.N;
+	}
+
+	/**
+	 * Search BST for given key. Does there exist a key-value pair with given key?
+	 * @param key the search key
+	 * @return true if key is found and false otherwise
+	 */
+	public boolean contains(Key key) {
+		return get(key) != null;
+	}
+
+	/**
+	 * Search BST for given key. What is the value associated with given key?
+	 * @param key the search key
+	 * @return value associated with the given key if found, or null if no such key
+	 *         exists.
+	 */
+	public Value get(Key key) {
+		return get(root, key);
+	}
+
+	private Value get(Node node, Key key) {
+		if (node == null)
+			return null;
+		int cmp = key.compareTo(node.key);
+		if (cmp < 0)
+			return get(node.left, key);
+		else if (cmp > 0)
+			return get(node.right, key);
+		else
+			return node.val;
+
+	}
+
+	/**
+	 * Insert key-value pair into BST. If key already exists, update with new value.
+	 * @param key the key to insert
+	 * @param val the value associated with key
+	 */
+	public void put(Key key, Value val) {
+		if (val == null) {
+			delete(key);
+			return;
+		}
+		root = put(root, key, val);
+	}
+
+	private Node put(Node node, Key key, Value val) {
+		if (node == null)
+			return new Node(key, val, 1); // new bst
+		int cmp = key.compareTo(node.key);
+		if (cmp < 0)
+			node.left = put(node.left, key, val);
+		else if (cmp > 0)
+			node.right = put(node.right, key, val);
+		else
+			node.val = val; // updating value
+		node.N = 1 + size(node.left) + size(node.right); // value child1 + value child2 + 1
+		return node;
+	}
+
+	/**
+	 * Tree height.
+	 *
+	 * @return the number of links from the root to the deepest leaf.
+	 */
+
+	public int height() {
+		return height(root);
+	}
+
+	private int height(Node x) {
+		if (x == null) {
+			return -1;
+		} else {
+			return 1 + Math.max(height(x.left), height(x.right));
+		}
+	}
+
+	/**
+	 * @return the median key, or null if the tree is empty.
+	 */
+	public Key median() {
+		if (isEmpty())
+			return null; 
+
+		else {
+			int median = (((size(root) + 1) / 2) - 1); // add 1 to size(root)
+			return intToKey(median);
+		}
+	}
+
+	private Key intToKey(int passedInt) {
+		Node node = intToKey(root, passedInt);
+		return node.key;
+	}
+
+	// find the node with equivalent key 
+	private Node intToKey(Node node, int passedInt) {
+		int leftSize = (size(node.left));
+
+		// check if it is in the left or right subtree
+		if (leftSize > passedInt) {
+			return intToKey(node.left, passedInt);
+		} else if (leftSize < passedInt) {
+			return intToKey(node.right, passedInt - leftSize - 1);
+		} else {
+			return node;
+		}
+	}
+
+	/**
+	 * Print all keys of the tree in a sequence, in-order. That is, for each node,
+	 * the keys in the left subtree should appear before the key in the node. Also,
+	 * for each node, the keys in the right subtree should appear before the key in
+	 * the node. For each subtree, its keys should appear within a parenthesis.
+	 *
+	 * @return a String with all keys in the tree, in order, parenthesized.
+	 */
+	public String printKeysInOrder() {
+		String res = "";
+		if (isEmpty()) {
+			return res += "()";
+		} else {
+			return res = printKeysInOrder(root);
+		}
+
+	}
+
+	private String printKeysInOrder(Node node) {
+		String res = "";
+		if (node == null) {
+			return res += "()";
+		}
+
+		else {
+			return res += ("(" + printKeysInOrder(node.left) + node.key.toString() + printKeysInOrder(node.right)
+					+ ")");
+		}
+
+	}
+
+	/**
+	 * Pretty Printing the tree. Each node is on one line 
+	 *
+	 * @return a multi-line string with the pretty ASCII picture of the tree.
+	 */
+	public String prettyPrintKeys() {
+		if (isEmpty())
+			return "-null\n";
+		return prettyPrint(root, "");
+	}
+
+	private String prettyPrint(Node node, String prefix) {
+		if (node == null) {
+			return (prefix + "-null\n");
+		} else {
+
+			return (prefix + "-" + node.key.toString() + "\n" + prettyPrint(node.left, (prefix + " |"))
+					+ prettyPrint(node.right, (prefix + "  ")));
+		}
+
+	}
+
+	/**
+	 * Deletes a key from a tree (if the key is in the tree). Note that this method
+	 * works symmetrically from the Hibbard deletion: If the node to be deleted has
+	 * two child nodes, then it needs to be replaced with its predecessor (not its
+	 * successor) node.
+	 *
+	 * @param key the key to delete
+	 */
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
+
+	private Node delete(Node node, Key key) {
+		if (node == null) {
+			return null;
+		}
+
+		int compare = key.compareTo(node.key);
+
+		if (compare > 0) {
+			node.right = delete(node.right, key);
+			node.left = delete(node.left, key);
+		} else if (compare < 0) {
+			node.left = delete(node.left, key);
+		} else {
+			if (node.right == null) {
+				return node.left;
+			}
+			if (node.left == null) {
+				return node.right;
+			}
+			Node temp = node;
+			node = max(temp.left);
+			node.left = deleteMax(temp.left);
+			node.right = temp.right;
+		}
+
+		node.N = size(node.left) + size(node.right) + 1;
+		return node;
+
+	}
+
+	private Node deleteMax(Node node) {
+		if (node.right == null)
+			return node.left;
+		node.right = deleteMax(node.right);
+		node.N = size(node.left) + size(node.right) + 1;
+		return node;
+	}
+
+	public Node max(Node node) {
+		if (node.right != null) {
+			return max(node.right);
+		}
+		return node;
+	}
+
+	public Key lowestCommonAncestor(Node node, Key key1, Key key2) {
+		if (node == null)
+			return null;
+		if (node.key == key1) {
+			return node.key;
+		}
+		if (node.key == key2) {
+			return node.key;
+		}
+		int cmp1 = node.key.compareTo(key1);
+		int cmp2 = node.key.compareTo(key2);
+
+		if (cmp1 >= 0 && cmp2 >= 0)
+			return lowestCommonAncestor(node.left, key1, key2);
+
+		if (cmp1 <= 0 && cmp2 <= 0)
+			return lowestCommonAncestor(node.right, key1, key2);
+
+		return node.key;
+	}
+
+	public static void main(String[] args) {
+		LCA<Integer, Integer> bst = new LCA<Integer, Integer>();
+
+		System.out.println(bst.lowestCommonAncestor(bst.root, 1, 2));
+		bst.put(7, 7); // _7_
+		bst.put(8, 8); // / \
+		bst.put(3, 3); // _3_ 8
+		bst.put(1, 1); // / \
+		bst.put(2, 2); // 1 6
+		bst.put(6, 6); // \ /
+		bst.put(4, 4); // 2 4
+		bst.put(5, 5); // \
+
+		System.out.print(bst.lowestCommonAncestor(bst.root, 2, 4));
 	}
 }
